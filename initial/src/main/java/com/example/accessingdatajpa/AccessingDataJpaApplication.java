a1fc9ac563c8f75d830dcfafe3ea93e445c350ab
Stanislav Roshchenko <rosenkostas@gmail.com>
2021-03-31 21:41:50 +0300

    Added Repository and modified Application class

--- initial/src/main/java/com/example/accessingdatajpa/AccessingDataJpaApplication.java
+++ initial/src/main/java/com/example/accessingdatajpa/AccessingDataJpaApplication.java
@@ -1,13 +1,54 @@
 package com.example.accessingdatajpa;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Optional;
 
 @SpringBootApplication
 public class AccessingDataJpaApplication {
 
+	private static final Logger LOG =
+			LoggerFactory.getLogger(AccessingDataJpaApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(AccessingDataJpaApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo(CustomerRepository repository) {
+
+		return (args) -> {
+			repository.save(new Customer("Stas", "Roshchenko"));
+			repository.save(new Customer("Bohan", "Tamirov"));
+			repository.save(new Customer("Mora", "Tsamyuk"));
+			repository.save(new Customer("Naya", "Rechenko"));
+
+			LOG.info("");
+			LOG.info("Find customers with findAll():");
+			LOG.info("------------------------------");
+			for (Customer c : repository.findAll()) {
+				LOG.info(c.toString());
+			}
+			LOG.info("");
+
+			Optional<Customer> customer = repository.findById(1L);
+			if (customer.isEmpty()) throw new IllegalStateException("No such user");
+			LOG.info("Find customer by ID:");
+			LOG.info("------------------------------");
+			LOG.info(customer.get().toString());
+			LOG.info("");
+
+			repository.findByLastName("Tamirov").forEach(c ->
+					LOG.info(c.toString())
+			);
+			LOG.info("");
+
+		};
 	}
 
 }
---
+++ initial/src/main/java/com/example/accessingdatajpa/CustomerRepository.java
@@ -0,0 +1,13 @@
+package com.example.accessingdatajpa;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    List<Customer> findByLastName(String lastName);
+
+}
